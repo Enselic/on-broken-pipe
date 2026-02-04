c090c9c6beeb955183e2249f240bc6a6062aa462
Martin Nordholts <martin.nordholts@codetale.se>
2026-02-04 06:50:54 +0100

    Add crates

---
+++ broken-pipe-errors/src/lib.rs
@@ -0,0 +1,9 @@
+// Copyright (c) 2026 Martin Nordholts
+// SPDX-License-Identifier: MIT
+
+#![feature(on_broken_pipe)]
+
+#[std::io::on_broken_pipe]
+fn on_broken_pipe() -> std::io::OnBrokenPipe {
+    std::io::OnBrokenPipe::Error
+}
